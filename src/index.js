6fc65c146cc3b345422fce923a9aba7af81298d5
PT-Otwtc-d8m7 <thanhauquang1987@gmail.com>
2024-06-14 13:13:38 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import routers from './apis';
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use('/', routers);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
